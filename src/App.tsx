5526d8e49f43c07341765c8f2c31f0d79c7a4ef6
Chequel McNeil <chequelmcneil@gmail.com>
2024-09-17 18:11:03 -0400

    Add About component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import About from './components/About'
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
         <div className='container mx-auto px-8'>
           <Navbar />
+          <About />
         </div>
       </div>  
     </div>
---
+++ src/components/About.tsx
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <div className='border-b border-neutral-900 pb-4 lg:mb-35'>
+      <div className='flex flex-wrap'>
+        <div className='w-full lg:w-1/2'>
+          <div className='flex flex-col items-center lg:items-start'>
+            <h1 className="pb-16 text-5xl font-thin tracking-tight lg:mt-16">Chequel McNeil</h1>
+            <span className="bg-gradient-to-r from-accent via-slate-400 to-green-500 bg-clip-text text-3xl tracking-tight text-transparent">
+              Full Stack Software Engineer
+            </span>
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter antialiased">
+            I am a results focused software engineer with 7+ years of experience in development, design, analysis, debugging, and implementation of enterprise level solutions. Well versed in a variety of technologies and languages utilized to build modern enterprise applications.
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default About
