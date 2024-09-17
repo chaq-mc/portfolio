48373b7afb879ffc0f6026273a9f4bdf8e44ba10
Chequel McNeil <chequelmcneil@gmail.com>
2024-09-17 16:50:49 -0400

    tailwind config setup and import default font

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <nav className="mb-20 flex items-center justify-between py-6">
+    <div className='flex flex-shrink-0 items-center'>
+        <h1 className='text-4xl font-semibold'>
+            Chequel<span>.</span>
+        </h1>
+    </div>
+   </nav>
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    screens: {
+       sm: '640px',
+       md: '768px',
+       lg: '960px',
+       xl: '1200px'
+    },
+    fontFamily: {
+      primary: 'JetBrains Mono'
+    },
+    extend: {
+      colors: {
+        primary: '#1c1c22'
+      }
+    },
+  },
+  plugins: [],
+}
