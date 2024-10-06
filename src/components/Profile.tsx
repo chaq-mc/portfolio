59f5147ce83a435860479b09b97ebc0f3dda41e1
Chequel McNeil <cmcne009@odu.edu>
2024-10-06 19:05:38 -0400

    Remove profile image to correct spacing of other components

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,7 @@ const Profile = () => {
       <div className='flex flex-wrap'>
         <div className='w-full lg:w-1/2'>
           <div className='flex flex-col items-center lg:items-start'>
-            <h1 className="pb-16 text-5xl font-thin tracking-tight lg:mt-16">Chequel McNeil</h1>
+            <h1 className="pb-16 text-3xl font-thin tracking-tight lg:mt-16 lg:text-6xl">Chequel McNeil</h1>
             <span className="bg-gradient-to-r from-accent via-slate-400 to-green-500 bg-clip-text text-3xl tracking-tight text-transparent">
               Full Stack Software Engineer
             </span>
@@ -15,11 +15,11 @@ const Profile = () => {
             </p>
           </div>
         </div>
-        <div className="w-full lg:w-1/2 lg:p-8">
+        {/* <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center h-screen">
             <img className="rounded-full h-96" src={profilePic} alt="Chequel McNeil" />
           </div>
-        </div>
+        </div> */}
       </div>
 
     </div>
