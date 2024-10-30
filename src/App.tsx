9abc1d3061fe8e9071c0399f284e0305d7d11a6c
Chequel McNeil <cmcne009@odu.edu>
2024-10-30 16:33:28 -0400

    Correct issue where bg color would not continue after scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Technologies from './components/Technologies'
 const App = () => {
   return (
     <div className='overflow-x-hidden antialiased'>
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+      <div className="absolute top-0 z-[-2] min-h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
         <div className='container mx-auto px-8'>
           <Navbar />
           <Profile />
