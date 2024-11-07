23f9f357ce166e1d732bc5b7bbe2d50d7fcdd8f2
Chequel McNeil <chequelmcneil@gmail.com>
2024-11-07 17:24:54 -0500

    Add Experience component

---
+++ src/constants/static.ts
@@ -0,0 +1,17 @@
+export const Experiences = [
+    {
+        year: "2020 - Present",
+        role: "Software Engineer II",
+        company: "Booz Allen Hamilton",
+        description: "",
+        technologies: [""],
+    },
+    {
+        year: "2017 - 2020",
+        role: "Software Engineer I",
+        company: "Booz Allen Hamilton",
+        description: "",
+        technologies: [""],
+    },
+
+]
