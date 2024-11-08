ab86ef7bae7b7624ff2bbbaafbb618ec8fa28778
Chequel McNeil <chequelmcneil@gmail.com>
2024-11-08 14:15:27 -0500

    Progress on adding in JSON for experiences

--- src/constants/static.ts
+++ src/constants/static.ts
@@ -3,14 +3,23 @@ export const Experiences = [
         year: "2020 - Present",
         role: "Software Engineer II",
         company: "Booz Allen Hamilton",
-        description: "",
-        technologies: [""],
+        description: [
+            "Led the development and modernization of the Data Access Viewer application for NASA POWER, transitioning the frontend stack to the React framework and incorporating modern web developement libraries to enhance functionality and performance.",
+            "Successfully launched a public beta, allowing users to engage with an improved interface and ensuring the application met NASA's standards for usability and reliability.",
+            "Directed a cross-functional team to deliver a successful beta version to the client, coordinating with stakeholders to ensure project milestones and objectives were met.",
+            "Focused on implementing scalable and maintainable code that adhered to best practices in modern front-end development, significantly enhancing the application’s user experience and responsiveness."
+        ],
+        technologies: ["React", "TypeScript", "Esri", "FastAPI"],
     },
     {
         year: "2017 - 2020",
         role: "Software Engineer I",
         company: "Booz Allen Hamilton",
-        description: "",
+        description: [
+            "Architected and developed key components while modernizing the existing ASP.NET Webforms application to a modern React front end, paired with an ASP.NET Core backend",
+            "Optimized application performance and ensured compatibility with current technologies, reducing maintenance requirements and improving application scalability.",
+            "Collaborated with cross-functional teams, including QA and product management, to ensure successful deployment of new features and adherence to project timelines."
+        ],
         technologies: [""],
     },
 
