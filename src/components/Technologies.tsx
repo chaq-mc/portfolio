30ddc71333b5803ed003852e4ae6d5d0c60dae3a
Chequel McNeil <chequelmcneil@gmail.com>
2024-11-13 14:16:58 -0500

    Convert tech icons to svg imports and update styling

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,24 +1,36 @@
-import { RiReactjsLine } from "react-icons/ri";
-import { TbBrandNextjs } from "react-icons/tb";
-import { TbBrandDocker } from "react-icons/tb";
-import { FaAws } from "react-icons/fa6";
+import awsIcon from "../assets/aws.svg";
+import dockerIcon from "../assets/docker.svg";
+import reactIcon from "../assets/react.svg";
+import nextjsIcon from "../assets/nextjs.svg";
+import typescriptIcon from "../assets/typescript.svg";
+import cssIcon from "../assets/css.svg";
+import htmlIcon from "../assets/html.svg";
 
 const Technologies = () => {
   return (
     <div className='border-b border-neutral-800 pb-24'>
       <h1 className='my-16 text-center text-4xl'>Technologies</h1>
       <div className='flex flex-wrap items-center justify-center gap-4'>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-          <RiReactjsLine className="text-7xl text-cyan-400"/>
+      <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+          <img className="w-16 h-16" src={typescriptIcon} alt=""/>
         </div>
         <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-          <TbBrandNextjs className="text-7xl"/>
+          <img className="w-16 h-16" src={htmlIcon} alt=""/>
         </div>
         <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-          <TbBrandDocker className="text-7xl text-blue-500"/>
+          <img className="w-16 h-16" src={cssIcon} alt=""/>
         </div>
         <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-          <FaAws className="text-7xl text-orange-400"/>
+          <img className="w-16 h-16" src={reactIcon} alt=""/>
+        </div>
+        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+          <img className="w-16 h-16" src={nextjsIcon} alt=""/>
+        </div>
+        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+          <img className="w-16 h-16" src={dockerIcon} alt=""/>
+        </div>
+        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+          <img className="w-16 h-16" src={awsIcon} alt=""/>
         </div>
       </div>
     </div>
