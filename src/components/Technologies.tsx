5f78f9aeb1d360f5b73bdd3bbdf064488e08cf77
Chequel McNeil <chequelmcneil@gmail.com>
2024-11-15 16:05:05 -0500

    Progress on technologies icons animations

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -5,15 +5,29 @@ import nextjsIcon from "../assets/nextjs.svg";
 import typescriptIcon from "../assets/typescript.svg";
 import cssIcon from "../assets/css.svg";
 import htmlIcon from "../assets/html.svg";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  hidden: { y: [-10] },
+  visible: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse"
+    }
+  }
+}) 
 
 const Technologies = () => {
   return (
     <div className='border-b border-neutral-800 pb-24'>
       <h1 className='my-16 text-center text-4xl'>Technologies</h1>
       <div className='flex flex-wrap items-center justify-center gap-4'>
-      <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+      <motion.div variants={iconVariants(2.5)} initial="hidden" animate="visible" className='rounded-2xl border-4 border-neutral-800 p-4'>
           <img className="w-16 h-16" src={typescriptIcon} alt=""/>
-        </div>
+        </motion.div>
         <div className='rounded-2xl border-4 border-neutral-800 p-4'>
           <img className="w-16 h-16" src={htmlIcon} alt=""/>
         </div>
