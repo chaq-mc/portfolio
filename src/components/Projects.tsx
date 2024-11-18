75bdb08d3469956b0585098f0bca3db30e72d47c
Chequel McNeil <chequelmcneil@gmail.com>
2024-11-18 16:20:12 -0500

    Animate Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,13 @@
+import { motion } from "framer-motion";
 import {PROJECTS} from "../constants/static";
 
 const Projects = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-        <h1 className="my-20 text-center text-4xl">Projects</h1>
+        <motion.h1 whileInView={{ opacity: 1, y:0 }} initial={{ opacity: 0, y: -100 }} transition={{ duration: 1 }} className="my-20 text-center text-4xl">Projects</motion.h1>
         <div className="grid grid-cols-1 gap-10 lg:grid-cols-2">
             {PROJECTS.map((proj, index) => (
-                 <div key={index} className="col-span-1">
+                 <motion.div whileInView={{ opacity: 1, x:0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1 }} key={index} className="col-span-1">
                     <img src={proj.image} className="rounded" />
                     <h3 className="text-lg font-semibold mt-1">{proj.title} </h3>
                     <div className="flex flex-wrap">
@@ -18,7 +19,7 @@ const Projects = () => {
                       <a href={proj.projLink} target="_blank" className="mr-6 mt-4 px-2 py-1 rounded border-b-4 border-green-500 hover:bg-neutral-900 hover:font-semibold">View Project</a>
                       {proj.hasGit ? <a href={proj.gitLink} target="_blank" className="mr-2 mt-4 px-2 py-1 rounded border-b-4 border-green-500 hover:bg-neutral-900 hover:font-semibold">View Code</a> : null}
                     </div>
-                </div>
+                </motion.div>
             ))}          
         </div>
     </div>
