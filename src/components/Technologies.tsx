daada12f7a5afa19f878ad7551b1b3499d809c5a
Chequel McNeil <chequelmcneil@gmail.com>
2024-11-18 15:24:23 -0500

    Add animations to technology icons

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -8,14 +8,14 @@ import htmlIcon from "../assets/html.svg";
 import { motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
-  hidden: { y: [-10] },
-  visible: {
+  initial: { y: [-10] },
+  animate: {
     y: [10, -10],
     transition: {
       duration: duration,
       ease: "linear",
       repeat: Infinity,
-      repeatType: "reverse"
+      repeatType: "reverse" as const,
     }
   }
 }) 
@@ -25,27 +25,27 @@ const Technologies = () => {
     <div className='border-b border-neutral-800 pb-24'>
       <h1 className='my-16 text-center text-4xl'>Technologies</h1>
       <div className='flex flex-wrap items-center justify-center gap-4'>
-      <motion.div variants={iconVariants(2.5)} initial="hidden" animate="visible" className='rounded-2xl border-4 border-neutral-800 p-4'>
+      <motion.div variants={iconVariants(2.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
           <img className="w-16 h-16" src={typescriptIcon} alt=""/>
         </motion.div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        <motion.div variants={iconVariants(2)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
           <img className="w-16 h-16" src={htmlIcon} alt=""/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div variants={iconVariants(6)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
           <img className="w-16 h-16" src={cssIcon} alt=""/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div variants={iconVariants(3)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
           <img className="w-16 h-16" src={reactIcon} alt=""/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div variants={iconVariants(4.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
           <img className="w-16 h-16" src={nextjsIcon} alt=""/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div variants={iconVariants(3.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
           <img className="w-16 h-16" src={dockerIcon} alt=""/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div variants={iconVariants(5.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
           <img className="w-16 h-16" src={awsIcon} alt=""/>
-        </div>
+        </motion.div>
       </div>
     </div>
   )
